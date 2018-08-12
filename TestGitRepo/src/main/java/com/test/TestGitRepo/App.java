7b9da21ab79712a4a2c07f4fa7ec63fd2e14510c
Geetha <Geetha@192.168.0.5>
2018-08-12 16:51:34 +0530

    First commit

---
+++ TestGitRepo/src/main/java/com/test/TestGitRepo/App.java
@@ -0,0 +1,13 @@
+package com.test.TestGitRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
